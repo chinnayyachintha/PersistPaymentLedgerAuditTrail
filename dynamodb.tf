ebe1d8628b63ed730e3d187992010bfcf23f62fa
Chinnayya Chintha <135199006+chinnayyachintha@users.noreply.github.com>
2024-11-28 13:11:25 +0530

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,9 +1,9 @@
 # payment_ledger
 resource "aws_dynamodb_table" "payment_ledger" {
-  name           = "${var.dynamodb_table_name}-Ledger"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "transaction_id"
-  range_key      = "process_type"
+  name         = "${var.dynamodb_table_name}-Ledger"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "transaction_id"
+  range_key    = "process_type"
 
   attribute {
     name = "transaction_id"
@@ -50,46 +50,74 @@ resource "aws_dynamodb_table" "payment_ledger" {
     type = "S"
   }
 
-  global_secondary_index {
-    name               = "payment_processor-index"
-    hash_key           = "payment_processor"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+  attribute {
+    name = "transaction_origin"
+    type = "S"
+  }
+
+  attribute {
+    name = "card_type"
+    type = "S"
   }
 
   global_secondary_index {
-    name               = "merchant_id-index"
-    hash_key           = "merchant_id"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "payment_processor-index"
+    hash_key        = "payment_processor"
+    range_key       = "timestamp"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "status-index"
-    hash_key           = "status"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "merchant_id-index"
+    hash_key        = "merchant_id"
+    range_key       = "timestamp"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "error_code-index"
-    hash_key           = "error_code"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "status-index"
+    hash_key        = "status"
+    range_key       = "timestamp"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "gateway_response-index"
-    hash_key           = "gateway_response"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "error_code-index"
+    hash_key        = "error_code"
+    range_key       = "timestamp"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "response_details-index"
-    hash_key           = "response_details"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "gateway_response-index"
+    hash_key        = "gateway_response"
+    range_key       = "timestamp"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "response_details-index"
+    hash_key        = "response_details"
+    range_key       = "timestamp"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "transaction_origin-index"
+    hash_key        = "transaction_origin"
+    range_key       = "timestamp"
+    projection_type = "ALL"
+  }
+
+  global_secondary_index {
+    name            = "card_type-index"
+    hash_key        = "card_type"
+    range_key       = "timestamp"
+    projection_type = "ALL"
+  }
+
+  point_in_time_recovery {
+    enabled = true
   }
 
   tags = {
@@ -97,14 +125,12 @@ resource "aws_dynamodb_table" "payment_ledger" {
   }
 }
 
-
 # payment_audit_trail
-
 resource "aws_dynamodb_table" "payment_audit_trail" {
-  name           = "${var.dynamodb_table_name}-AuditTrail"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "audit_id"
-  range_key      = "transaction_id"
+  name         = "${var.dynamodb_table_name}-AuditTrail"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "audit_id"
+  range_key    = "transaction_id"
 
   attribute {
     name = "audit_id"
@@ -152,45 +178,49 @@ resource "aws_dynamodb_table" "payment_audit_trail" {
   }
 
   global_secondary_index {
-    name               = "transaction_id-index"
-    hash_key           = "transaction_id"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "transaction_id-index"
+    hash_key        = "transaction_id"
+    range_key       = "timestamp"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "user_id-index"
-    hash_key           = "user_id"
-    range_key          = "action_type"
-    projection_type    = "ALL"
+    name            = "user_id-index"
+    hash_key        = "user_id"
+    range_key       = "action_type"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "action_details-index"
-    hash_key           = "action_details"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "action_details-index"
+    hash_key        = "action_details"
+    range_key       = "timestamp"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "error_code-index"
-    hash_key           = "error_code"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "error_code-index"
+    hash_key        = "error_code"
+    range_key       = "timestamp"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "payment_result-index"
-    hash_key           = "payment_result"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "payment_result-index"
+    hash_key        = "payment_result"
+    range_key       = "timestamp"
+    projection_type = "ALL"
   }
 
   global_secondary_index {
-    name               = "source_ip-index"
-    hash_key           = "source_ip"
-    range_key          = "timestamp"
-    projection_type    = "ALL"
+    name            = "source_ip-index"
+    hash_key        = "source_ip"
+    range_key       = "timestamp"
+    projection_type = "ALL"
+  }
+
+  point_in_time_recovery {
+    enabled = true
   }
 
   tags = {
